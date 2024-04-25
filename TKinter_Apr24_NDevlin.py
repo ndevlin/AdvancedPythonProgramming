b08e5351774f6a3a1f7b6b8bcb84670f200933f5
Nathan Devlin <ndevlin@seas.upenn.edu>
2024-04-25 12:22:59 -0700

    Got basic functionality working

--- TKinter_Apr24_NDevlin.py
+++ TKinter_Apr24_NDevlin.py
@@ -4,10 +4,91 @@ TKinter
 April 24th
 '''
 
+from collections import defaultdict
+from urllib.request import urlopen
+import re
+from bs4 import BeautifulSoup
+
+import numpy as np
+import pandas as pd
 
 import tkinter as tk
 from tkinter import ttk
-import pandas as pd
+
+class WebScraper():
+
+    def __init__(self):
+        self.soup = None
+        self.memberDict = defaultdict(list)
+        self.dataFrame = None
+
+    def requestWebPage(self, pageIn):
+        try:
+            html = urlopen(pageIn)
+            self.soup =  BeautifulSoup(html, "html.parser")
+        except:
+            print("Issue retrieving Web Page")
+            self.soup = None
+
+    def webPageFromFile(self, fileName):
+        try:
+            with open(fileName, "r") as file:
+                html = file.read()
+                self.soup =  BeautifulSoup(html, "html.parser")
+        except:
+            print("Issue retrieving Web Page")
+            self.soup = None
+
+    def extractTags(self, tagIn):
+        if self.soup == None:
+            print("Webpage was not loaded")
+            return
+        
+        tagResult = None
+        tagResult = self.soup.select(tagIn)
+        if tagResult == None:
+            print("No tags were found")
+            return
+
+        output = []
+
+        for i in range(0, len(tagResult)):
+            output.append(str((tagResult)[i].text))
+
+        return (tagIn, tuple(output))
+
+    def cleanTags(self, tagTupleIn):
+        self.memberDict[tagTupleIn[0]] = tagTupleIn[1]
+
+    def getData(self):
+        return self.memberDict.items()
+
+    def convertToPandasDataFrame(self):
+        if self.memberDict == defaultdict(list):
+            print("Must Clean Tags before converting to Pandas DataFrame")
+            return
+        
+        try:
+            headers = self.memberDict["tr"][0]
+            numRows = int(re.search(r'\n(\d+)\n', self.memberDict["tr"][-1]).group(1))
+            headers = headers.split("\n")
+            while "" in headers:
+                headers.remove("")
+            numCols = len(headers)
+            pandaReadyList = []
+            for i in range(numRows):
+                row = []
+                for j in range(numCols):
+                    currVal = i * numCols + j
+                    row.append(self.memberDict["td"][currVal])
+                pandaReadyList.append(row)
+            self.dataFrame = pd.DataFrame(pandaReadyList, columns = headers)
+        except:
+            print("Conversion to DataFrame failed")
+    
+    def printDataFrame(self):
+        print(self.dataFrame)
+
 
 def display_dataframe(df):
     root = tk.Tk()
@@ -50,4 +131,20 @@ df = pd.DataFrame({
     'C': [7, 8, 9]
 })
 
+webScraper = WebScraper()
+
+webScraper.webPageFromFile("GHGEmissionsTestVersion.html")
+
+relevantTags = ["th", "tr", "td"]
+
+for tag in relevantTags:
+    tableData = webScraper.extractTags(tag)
+    webScraper.cleanTags(tableData)
+
+webScraper.convertToPandasDataFrame()
+webScraper.printDataFrame()
+
+df = webScraper.dataFrame
+
 display_dataframe(df)
+
