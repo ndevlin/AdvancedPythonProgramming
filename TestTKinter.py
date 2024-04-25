08104f50caf4897a22390530486a4204332f99cb
Nathan Devlin <ndevlin@seas.upenn.edu>
2024-04-25 11:28:39 -0700

    Update test files

--- TestTKinter.py
+++ TestTKinter.py
@@ -10,14 +10,14 @@ from tkinter import *
 # tkinter datatable chart
 def Chart(name, data):
     import pandas as pd
-    from pandastable import Table, TableModel
+    #from pandastable import Table, TableModel
     window = Tk()
     window.title(name)
     window.geometry('800x600')
     frame = Frame(window)
     frame.pack(expand=True, fill=BOTH)
-    widget = Table(frame, dataframe=data, showtoolbar=False, showstatusbar=False)
-    widget.show()
+    #widget = Table(frame, dataframe=data, showtoolbar=False, showstatusbar=False)
+    #widget.show()
     window.mainloop()
     
 dfcsv = pd.read_csv("dwarfs.csv")
