db9604cab26ced4eac2ff36f153865b4f9c0df80
Nathan Devlin <ndevlin@alumni.upenn.edu>
2024-05-01 19:09:42 +0000

    Add class functionality

--- CaesarEncryptionAndDecryptionM1Q.py
+++ CaesarEncryptionAndDecryptionM1Q.py
@@ -3,40 +3,48 @@ Rotating Caesar Cypher encryption and decryption
 Mid 1 Q
 '''
 
-def caesarCypher(inputString, shift):
-    outputString = ""
-    for char in inputString:
-        asciiVal = ord(char)
-        newAsciiVal = asciiVal + shift
-        if newAsciiVal > 0x80:
-            newAsciiVal = newAsciiVal - 0x80 + 0x20 - 1
-        outputString += chr(newAsciiVal)
-        shift += 1
-    return outputString
+class Decryption:
 
+    def __init__(self, shift):
+        self.shift = shift
 
-def decryptCaesarCypher(inputString, shift):
-    outputString = ""
-    for char in inputString:
-        asciiVal = ord(char)
-        newAsciiVal = asciiVal - shift
-        if newAsciiVal < 0x20:
-            newAsciiVal = newAsciiVal + 0x80 - 0x20 + 1
-        outputString += chr(newAsciiVal)
-        shift += 1
-    return outputString
+    def caesarCypher(self, inputString):
+        outputString = ""
+        shift = self.shift
+        for char in inputString:
+            asciiVal = ord(char)
+            newAsciiVal = asciiVal + shift
+            if newAsciiVal > 0x80:
+                newAsciiVal = newAsciiVal - 0x80 + 0x20 - 1
+            outputString += chr(newAsciiVal)
+            shift += 1
+        return outputString
+
+    def decryptCaesarCypher(self, inputString):
+        outputString = ""
+        shift = self.shift
+        for char in inputString:
+            asciiVal = ord(char)
+            newAsciiVal = asciiVal - shift
+            if newAsciiVal < 0x20:
+                newAsciiVal = newAsciiVal + 0x80 - 0x20 + 1
+            outputString += chr(newAsciiVal)
+            shift += 1
+        return outputString
 
 
 # Main
 
 stringToEncrypt = "HELLO"
-shift = 5
+
+decrypter = Decryption(shift=5)
+
 print("Encrypt", stringToEncrypt)
-encryptedString = caesarCypher(stringToEncrypt, shift)
+encryptedString = decrypter.caesarCypher(stringToEncrypt)
 print(encryptedString)
 
 print("Decrypt ", encryptedString)
-decryptedString = decryptCaesarCypher(encryptedString, shift)
+decryptedString = decrypter.decryptCaesarCypher(encryptedString)
 print(decryptedString)
 
 
