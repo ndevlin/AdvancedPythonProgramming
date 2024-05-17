bca2ef037bd3a3d18e9a34f05f09c6064fd89bd7
Nathan Devlin <ndevlin@alumni.upenn.edu>
2024-05-17 08:26:47 +0000

    Added bitstring functionality

--- ByteStreams_May15_NDevlin.py
+++ ByteStreams_May15_NDevlin.py
@@ -1,19 +1,25 @@
 import binascii
 import unittest
 import socket
+import bitstring
 
 class ByteStream:
     def __init__(self, data, isBinary=False):
-        if isBinary:
+        if data is None:
+            self._byte = b''
+            self._byte_array = bytearray()
+            self._bitstring = bitstring.BitArray()
+            self._string = None
+        elif isBinary:
             self._byte = data
             self._byte_array = bytearray(data)
-            self._bitstring = bin(int(binascii.hexlify(self._byte), 16))
+            self._bitstring = bitstring.BitArray(bytes=data)
             self._string = None
         else:
             self._string = data
             self._byte = self._string.encode()
             self._byte_array = bytearray(self._string, 'utf-8')
-            self._bitstring = bin(int(binascii.hexlify(self._byte), 16))
+            self._bitstring = bitstring.BitArray(bytes=self._byte)
 
     def __str__(self):
         return self._string if self._string else self._byte.decode()
@@ -46,21 +52,19 @@ class ByteStream:
         return self._byte_array
 
     def get_bitstring(self):
-        return self._bitstring
+        return self._bitstring.bin
 
-    def byte_to_string(self, byte):
-        return byte.decode()
+    def byte_to_string(self):
+        return self._byte.decode()
 
-    def byte_array_to_string(self, byte_array):
-        return byte_array.decode()
+    def byte_array_to_string(self):
+        return bytes(self._byte_array).decode()
 
-    def bitstring_to_string(self, bitstring):
-        n = int(bitstring, 2)
-        return binascii.unhexlify('%x' % n).decode()
+    def bitstring_to_string(self):
+        return self._bitstring.bytes.decode()
 
-    def bitstring_to_byte(self, bitstring):
-        byte_string = int(bitstring, 2).to_bytes((len(bitstring) + 7) // 8, 'big')
-        return byte_string.lstrip(b'\x00')
+    def bitstring_to_byte(self):
+        return self._bitstring.bytes
 
     def send_byte_stream(self, byte_stream, socket):
         conn, _ = socket.accept()
@@ -77,6 +81,33 @@ class ByteStream:
             conn.close()
     
 
+
+    # Probably wont use:
+    def send(self, host, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.sendall(self._byte)
+
+    def receive(self, host, port):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, port))
+            s.listen()
+
+            # Set a timeout
+            s.settimeout(2.0)
+
+            try:
+                conn, addr = s.accept()
+                with conn:
+                    data = conn.recv(1024)
+                    self._byte = data
+                    self._byte_array = bytearray(data)
+                    self._bitstring = bitstring.BitArray(bytes=data)
+                    self._string = None
+            except socket.timeout:
+                print("Socket timed out")
+    
+
 class TestByteStream(unittest.TestCase):
     def setUp(self):
         self.converter = ByteStream("Hello, World!")
@@ -85,8 +116,14 @@ class TestByteStream(unittest.TestCase):
     def test_byte_conversion(self):
         byte = self.converter.get_byte()
         print(f"Testing byte conversion: {byte}")
-        self.assertEqual(self.converter.byte_to_string(byte), "Hello, World!")
+        self.assertEqual(self.converter.byte_to_string(), "Hello, World!")
         print("Passed byte conversion test")
+
+    def test_bitstring_conversion(self):
+        bitstring = self.converter.get_bitstring()
+        print(f"Testing bitstring conversion: {bitstring}")
+        self.assertEqual(self.converter.bitstring_to_byte(), self.converter.get_byte())
+        print("Passed bitstring conversion test")
 
     def test_len(self):
         print(f"Testing length: {len(self.converter)}")
@@ -112,12 +149,6 @@ class TestByteStream(unittest.TestCase):
         print("Testing contains: Checking if 74 (ASCII value of 'J') is not in ByteStream")
         self.assertFalse(74 in self.converter)
         print("Passed contains test for 'J'")
-
-    def test_bitstring_conversion(self):
-        bitstring = self.converter.get_bitstring()
-        print(f"Testing bitstring conversion: {bitstring}")
-        self.assertEqual(self.converter.bitstring_to_byte(bitstring), self.converter.get_byte())
-        print("Passed bitstring conversion test")
 
     def test_send_byte_stream(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -145,6 +176,46 @@ class TestByteStream(unittest.TestCase):
                 self.assertEqual(data, self.converter.get_byte())
                 print("Passed receive byte stream test")
 
+    
+    # Probably wont use:
+    '''
+    def test_send_and_receive(self):
+
+        import threading
+
+        # Create an Event to signal when the server has started
+        server_started = threading.Event()
+
+        # Start a server in a separate thread
+        server_thread = threading.Thread(target=self.start_server, args=(server_started,))
+        server_thread.start()
+
+        # Wait for the server to start
+        server_started.wait()
+
+        # Send data
+        self.converter.send('127.0.0.1', 12345)
+
+        # Wait for the server to finish
+        server_thread.join()
+
+    def start_server(self, server_started):
+    # Create a new ByteStream object to receive data
+        receiver = ByteStream(b'', isBinary=True)
+
+        try:
+            # Start listening for connections
+            receiver.receive('127.0.0.1', 12345)
+
+            # Signal that the server has started
+            server_started.set()
+
+            # Check that the received data matches the sent data
+            self.assertEqual(receiver.get_byte(), self.converter.get_byte())
+        finally:
+            # Close the server socket
+            receiver.close()
+    '''
 
 unittest.main()
     
