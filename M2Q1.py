25e596237b5235ad147505ff3700a49732d45e84
Nathan Devlin <ndevlin@alumni.upenn.edu>
2024-06-05 20:13:55 +0000

    Converted to make base-prefixed string default version of the numbers

--- M2Q1.py
+++ M2Q1.py
@@ -19,46 +19,51 @@ class BaseConverter:
         reverseMap[v] = k
 
     '''
-    Convert a number in a given base to decimal
-    number: str, number in the given base in string form
-    base: int, base of the number
+    Convert a number in a given base to decimal numeric form
+    numberIn: str, number in the given base in string form with prefix
+    return: int, decimal numeric form of the number
     '''
-    def toDecimal(self, numberIn, base):
-        base_10 = 0
-        for i, digit in enumerate(reversed(numberIn)):
+    def toDecimal(self, numberIn):
+        number, base = self.parseRepresentation(numberIn)
+        decimal = 0
+        for i, digit in enumerate(reversed(number)):
             currValue = self.baseMap[digit]
-            base_10 += currValue * (base ** i)
-        return base_10
+            decimal += currValue * (base ** i)
+        return decimal
 
     '''
     Convert a decimal number to a given base
-    numberIn: int, decimal number
-    base: int, base to convert to
+    numberIn: int, decimal numeric number
+    toBase: int, base to convert to
+    return: str, number in the new base in string form with prefix
     '''
-    def fromDecimal(self, numberIn, base):
+    def fromDecimal(self, numberIn, toBase):
         digits = []
         number = numberIn
         while number > 0:
-            number, remainder = divmod(number, base)
+            number, remainder = divmod(number, toBase)
             digits.append(self.reverseMap[remainder])
         digits = reversed(digits)
         result = ''.join(digits)
-        return result
+        return self.represent(result, toBase)
 
     '''
     Convert a number from one base to another
-    number: str, number in its own base in string form
-    fromBase: int, base of the incoming number
+    numberIn: str, number in its own base in string form with prefix
     toBase: int, base to convert to
+    return: str, number in the new base in string form
     '''
-    def convert(self, number, fromBase, toBase):
-        decimal = self.toDecimal(number, fromBase)
-        return self.fromDecimal(decimal, toBase)
+    def convert(self, numberIn, toBase):
+        decimal = self.toDecimal(numberIn)
+        newVersion = self.fromDecimal(decimal, toBase)
+        return newVersion
 
     '''
     Create a string representation of a number indicating its base
-    number: str, number in its own base in string form, without prepended base indicator
+    Base indicators are represented as "0", base character, "_", e.g. "02_", "03_", ..., "0A_", "0B_", ...
+    number: str, number in its own base in string form, without prefixed base indicator
     base: int, base of the number
+    return: str, base-prefixed string representation of the number
     '''
     def represent(self, number, base):
         result = "0" + self.reverseMap[base] + "_" + number
@@ -70,6 +75,7 @@ class BaseConverter:
     and returns the number in string form and its base in decimal numeric form
     as a tuple, e.g. ('36', 10)
     representation: str, base-prefixed string representation of the number
+    return: tuple of (str, int), number in string form and its base in decimal numeric form
     '''
     def parseRepresentation(self, representation):
         baseChar, number = representation.split('_')
@@ -83,6 +89,7 @@ class TestBaseConverter(unittest.TestCase):
     def setUp(self):
         self.converter = BaseConverter()
 
+
     def test_parseRepresentation(self):
         self.assertEqual(self.converter.parseRepresentation('02_100100'), ('100100', 2))
         self.assertEqual(self.converter.parseRepresentation('08_44'), ('44', 8))
@@ -93,13 +100,13 @@ class TestBaseConverter(unittest.TestCase):
         print('\n', "test_parseRepresentation passed")
 
     def test_convert(self):
-        self.assertEqual(self.converter.convert('100100', 2, 16), '24')
-        self.assertEqual(self.converter.convert('44', 8, 10), '36')
-        self.assertEqual(self.converter.convert('36', 10, 2), '100100')
-        self.assertEqual(self.converter.convert('24', 16, 8), '44')
-        self.assertEqual(self.converter.convert('13', 33, 35), '11')
-        self.assertEqual(self.converter.convert('12', 34, 35), '11')
-        self.assertEqual(self.converter.convert('11', 35, 2), '100100')
+        self.assertEqual(self.converter.convert('02_100100', 16), '0G_24')
+        self.assertEqual(self.converter.convert('08_44', 10), '0A_36')
+        self.assertEqual(self.converter.convert('0A_36', 2), '02_100100')
+        self.assertEqual(self.converter.convert('0G_24', 8), '08_44')
+        self.assertEqual(self.converter.convert('0X_13', 35), '0Z_11')
+        self.assertEqual(self.converter.convert('0Y_12', 35), '0Z_11')
+        self.assertEqual(self.converter.convert('0Z_11', 2), '02_100100')
         print('\n', "test_convert passed")
 
     def test_represent(self):
@@ -113,32 +120,32 @@ class TestBaseConverter(unittest.TestCase):
         print('\n', "test_represent passed")
 
     def test_toDecimal(self):
-        self.assertEqual(self.converter.toDecimal('100100', 2), 36)
-        self.assertEqual(self.converter.toDecimal('44', 8), 36)
-        self.assertEqual(self.converter.toDecimal('36', 10), 36)
-        self.assertEqual(self.converter.toDecimal('24', 16), 36)
-        self.assertEqual(self.converter.toDecimal('13', 33), 36)
-        self.assertEqual(self.converter.toDecimal('12', 34), 36)
-        self.assertEqual(self.converter.toDecimal('11', 35), 36)
+        self.assertEqual(self.converter.toDecimal('02_100100'), 36)
+        self.assertEqual(self.converter.toDecimal('08_44'), 36)
+        self.assertEqual(self.converter.toDecimal('0A_36'), 36)
+        self.assertEqual(self.converter.toDecimal('0G_24'), 36)
+        self.assertEqual(self.converter.toDecimal('0X_13'), 36)
+        self.assertEqual(self.converter.toDecimal('0Y_12'), 36)
+        self.assertEqual(self.converter.toDecimal('0Z_11'), 36)
         print('\n', "test_toDecimal passed")
 
     def test_decimal_36(self):
-        self.assertEqual(self.converter.fromDecimal(36, 2), '100100')
-        self.assertEqual(self.converter.fromDecimal(36, 8), '44')
-        self.assertEqual(self.converter.fromDecimal(36, 10), '36')
-        self.assertEqual(self.converter.fromDecimal(36, 16), '24')
-        self.assertEqual(self.converter.fromDecimal(36, 33), '13')
-        self.assertEqual(self.converter.fromDecimal(36, 34), '12')
-        self.assertEqual(self.converter.fromDecimal(36, 35), '11')
+        self.assertEqual(self.converter.fromDecimal(36, 2), '02_100100')
+        self.assertEqual(self.converter.fromDecimal(36, 8), '08_44')
+        self.assertEqual(self.converter.fromDecimal(36, 10), '0A_36')
+        self.assertEqual(self.converter.fromDecimal(36, 16), '0G_24')
+        self.assertEqual(self.converter.fromDecimal(36, 33), '0X_13')
+        self.assertEqual(self.converter.fromDecimal(36, 34), '0Y_12')
+        self.assertEqual(self.converter.fromDecimal(36, 35), '0Z_11')
         print('\n', "test_decimal_36 passed")
 
     def test_decimal_123(self):
-        self.assertEqual(self.converter.fromDecimal(123, 2), '1111011')
-        self.assertEqual(self.converter.fromDecimal(123, 8), '173')
-        self.assertEqual(self.converter.fromDecimal(123, 16), '7B')
-        self.assertEqual(self.converter.fromDecimal(123, 26), '4J')
-        self.assertEqual(self.converter.fromDecimal(123, 33), '3O')
-        self.assertEqual(self.converter.fromDecimal(123, 35), '3I')
+        self.assertEqual(self.converter.fromDecimal(123, 2), '02_1111011')
+        self.assertEqual(self.converter.fromDecimal(123, 8), '08_173')
+        self.assertEqual(self.converter.fromDecimal(123, 16), '0G_7B')
+        self.assertEqual(self.converter.fromDecimal(123, 26), '0Q_4J')
+        self.assertEqual(self.converter.fromDecimal(123, 33), '0X_3O')
+        self.assertEqual(self.converter.fromDecimal(123, 35), '0Z_3I')
         print('\n', "test_decimal_123 passed")
         
 # Run tests
