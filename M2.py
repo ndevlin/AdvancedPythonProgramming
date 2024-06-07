14fad6a281627eb307e8152fe0ff5f90f0add64a
Nathan Devlin <ndevlin@alumni.upenn.edu>
2024-06-07 02:23:40 +0000

    Moved test code

--- M2.py
+++ M2.py
@@ -116,121 +116,6 @@ class BaseConverter:
         return number, base
 
 
-'''
-# Base Converter Testing code
-class TestBaseConverter(unittest.TestCase):
-    def setUp(self):
-        self.converter = BaseConverter()
-
-    def test_getBaseInDecimal(self):
-        self.assertEqual(self.converter.getBaseInDecimal('2'), 2)
-        self.assertEqual(self.converter.getBaseInDecimal('8'), 8)
-        self.assertEqual(self.converter.getBaseInDecimal('A'), 10)
-        self.assertEqual(self.converter.getBaseInDecimal('G'), 16)
-        self.assertEqual(self.converter.getBaseInDecimal('O'), 24)
-        self.assertEqual(self.converter.getBaseInDecimal('P'), 25)
-        self.assertEqual(self.converter.getBaseInDecimal(2), 2)
-        self.assertEqual(self.converter.getBaseInDecimal(8), 8)
-        self.assertEqual(self.converter.getBaseInDecimal(10), 10)
-        self.assertEqual(self.converter.getBaseInDecimal(16), 16)
-        self.assertEqual(self.converter.getBaseInDecimal(24), 24)
-        self.assertEqual(self.converter.getBaseInDecimal(25), 25)
-        print('\n', "test_getBaseInDecimal passed")
-
-    def test_parseRepresentation(self):
-        self.assertEqual(self.converter.parseRepresentation('02_100100'), ('100100', 2))
-        self.assertEqual(self.converter.parseRepresentation('08_44'), ('44', 8))
-        self.assertEqual(self.converter.parseRepresentation('0A_36'), ('36', 10))
-        self.assertEqual(self.converter.parseRepresentation('0G_24'), ('24', 16))
-        self.assertEqual(self.converter.parseRepresentation('0O_13'), ('13', 24))
-        self.assertEqual(self.converter.parseRepresentation('0P_12'), ('12', 25))
-        print('\n', "test_parseRepresentation passed")
-
-    def test_convert(self):
-        self.assertEqual(self.converter.convert('02_100100', "G"), '0G_24')
-        self.assertEqual(self.converter.convert('08_44', 10), '0A_36')
-        self.assertEqual(self.converter.convert('0A_36', 2), '02_100100')
-        self.assertEqual(self.converter.convert('0G_24', 8), '08_44')
-        self.assertEqual(self.converter.convert('0X_13', "Z"), '0Z_11')
-        self.assertEqual(self.converter.convert('0Y_12', 35), '0Z_11')
-        self.assertEqual(self.converter.convert('0Z_11', 2), '02_100100')
-        print('\n', "test_convert passed")
-
-    def test_represent(self):
-        self.assertEqual(self.converter.represent('100100', 2), '02_100100')
-        self.assertEqual(self.converter.represent('44', 8), '08_44')
-        self.assertEqual(self.converter.represent('36', 10), '0A_36')
-        self.assertEqual(self.converter.represent('24', 16), '0G_24')
-        self.assertEqual(self.converter.represent('12', 24), '0O_12')
-        self.assertEqual(self.converter.represent('11', 25), '0P_11')
-        self.assertEqual(self.converter.represent('10', 35), '0Z_10')
-        print('\n', "test_represent passed")
-
-    def test_toDecimal(self):
-        self.assertEqual(self.converter.toDecimal('02_100100'), 36)
-        self.assertEqual(self.converter.toDecimal('08_44'), 36)
-        self.assertEqual(self.converter.toDecimal('0A_36'), 36)
-        self.assertEqual(self.converter.toDecimal('0G_24'), 36)
-        self.assertEqual(self.converter.toDecimal('0X_13'), 36)
-        self.assertEqual(self.converter.toDecimal('0Y_12'), 36)
-        self.assertEqual(self.converter.toDecimal('0Z_11'), 36)
-        print('\n', "test_toDecimal passed")
-
-    def test_decimal_36(self):
-        self.assertEqual(self.converter.fromDecimal(36, 2), '02_100100')
-        self.assertEqual(self.converter.fromDecimal(36, 8), '08_44')
-        self.assertEqual(self.converter.fromDecimal(36, 10), '0A_36')
-        self.assertEqual(self.converter.fromDecimal(36, 16), '0G_24')
-        self.assertEqual(self.converter.fromDecimal(36, 33), '0X_13')
-        self.assertEqual(self.converter.fromDecimal(36, 34), '0Y_12')
-        self.assertEqual(self.converter.fromDecimal(36, 35), '0Z_11')
-        print('\n', "test_decimal_36 passed")
-
-    def test_decimal_123(self):
-        self.assertEqual(self.converter.fromDecimal(123, 2), '02_1111011')
-        self.assertEqual(self.converter.fromDecimal(123, 8), '08_173')
-        self.assertEqual(self.converter.fromDecimal(123, 16), '0G_7B')
-        self.assertEqual(self.converter.fromDecimal(123, 26), '0Q_4J')
-        self.assertEqual(self.converter.fromDecimal(123, 33), '0X_3O')
-        self.assertEqual(self.converter.fromDecimal(123, 35), '0Z_3I')
-        print('\n', "test_decimal_123 passed")
-
-# Main Q1
-if __name__ == '__main__':
-
-    converter = BaseConverter()
-
-    numbers = [36, 123, 456]
-
-    bases = [2, 8, 16]
-
-    for number in numbers:
-        print(f"\nTesting number {number}:")
-
-        currNumber = converter.represent(str(number), 10)
-
-        # Convert the number to each base
-        for base in bases:
-            print(f"\nConverting {currNumber} to base {base}:")
-            numInBase = converter.convert(currNumber, base)
-            print(f"Number {currNumber} in base {base} is {numInBase}")
-            currNumber = numInBase
-
-        print(f"\nConverting {currNumber} back to base 10:")
-        numInDecimal = converter.toDecimal(currNumber)
-        print(f"Number {currNumber} is {numInDecimal} in decimal")
-
-        # Check if the number is the same after converting back to base 10
-        if numInDecimal == number:
-            print(f"Success: {numInDecimal} == {numInDecimal}")
-        else:
-            print(f"Failure: {numInDecimal} != {numInDecimal}")
-
-    # Run the unit tests
-    unittest.main()
-'''
-
-
 """
 2
 """
@@ -324,6 +209,87 @@ class BaseXNumber:
 
 
 
+
+# Base Converter Testing code
+class TestBaseConverter(unittest.TestCase):
+    def setUp(self):
+        self.converter = BaseConverter()
+
+    def test_getBaseInDecimal(self):
+        self.assertEqual(self.converter.getBaseInDecimal('2'), 2)
+        self.assertEqual(self.converter.getBaseInDecimal('8'), 8)
+        self.assertEqual(self.converter.getBaseInDecimal('A'), 10)
+        self.assertEqual(self.converter.getBaseInDecimal('G'), 16)
+        self.assertEqual(self.converter.getBaseInDecimal('O'), 24)
+        self.assertEqual(self.converter.getBaseInDecimal('P'), 25)
+        self.assertEqual(self.converter.getBaseInDecimal(2), 2)
+        self.assertEqual(self.converter.getBaseInDecimal(8), 8)
+        self.assertEqual(self.converter.getBaseInDecimal(10), 10)
+        self.assertEqual(self.converter.getBaseInDecimal(16), 16)
+        self.assertEqual(self.converter.getBaseInDecimal(24), 24)
+        self.assertEqual(self.converter.getBaseInDecimal(25), 25)
+        print('\n', "test_getBaseInDecimal passed")
+
+    def test_parseRepresentation(self):
+        self.assertEqual(self.converter.parseRepresentation('02_100100'), ('100100', 2))
+        self.assertEqual(self.converter.parseRepresentation('08_44'), ('44', 8))
+        self.assertEqual(self.converter.parseRepresentation('0A_36'), ('36', 10))
+        self.assertEqual(self.converter.parseRepresentation('0G_24'), ('24', 16))
+        self.assertEqual(self.converter.parseRepresentation('0O_13'), ('13', 24))
+        self.assertEqual(self.converter.parseRepresentation('0P_12'), ('12', 25))
+        print('\n', "test_parseRepresentation passed")
+
+    def test_convert(self):
+        self.assertEqual(self.converter.convert('02_100100', "G"), '0G_24')
+        self.assertEqual(self.converter.convert('08_44', 10), '0A_36')
+        self.assertEqual(self.converter.convert('0A_36', 2), '02_100100')
+        self.assertEqual(self.converter.convert('0G_24', 8), '08_44')
+        self.assertEqual(self.converter.convert('0X_13', "Z"), '0Z_11')
+        self.assertEqual(self.converter.convert('0Y_12', 35), '0Z_11')
+        self.assertEqual(self.converter.convert('0Z_11', 2), '02_100100')
+        print('\n', "test_convert passed")
+
+    def test_represent(self):
+        self.assertEqual(self.converter.represent('100100', 2), '02_100100')
+        self.assertEqual(self.converter.represent('44', 8), '08_44')
+        self.assertEqual(self.converter.represent('36', 10), '0A_36')
+        self.assertEqual(self.converter.represent('24', 16), '0G_24')
+        self.assertEqual(self.converter.represent('12', 24), '0O_12')
+        self.assertEqual(self.converter.represent('11', 25), '0P_11')
+        self.assertEqual(self.converter.represent('10', 35), '0Z_10')
+        print('\n', "test_represent passed")
+
+    def test_toDecimal(self):
+        self.assertEqual(self.converter.toDecimal('02_100100'), 36)
+        self.assertEqual(self.converter.toDecimal('08_44'), 36)
+        self.assertEqual(self.converter.toDecimal('0A_36'), 36)
+        self.assertEqual(self.converter.toDecimal('0G_24'), 36)
+        self.assertEqual(self.converter.toDecimal('0X_13'), 36)
+        self.assertEqual(self.converter.toDecimal('0Y_12'), 36)
+        self.assertEqual(self.converter.toDecimal('0Z_11'), 36)
+        print('\n', "test_toDecimal passed")
+
+    def test_decimal_36(self):
+        self.assertEqual(self.converter.fromDecimal(36, 2), '02_100100')
+        self.assertEqual(self.converter.fromDecimal(36, 8), '08_44')
+        self.assertEqual(self.converter.fromDecimal(36, 10), '0A_36')
+        self.assertEqual(self.converter.fromDecimal(36, 16), '0G_24')
+        self.assertEqual(self.converter.fromDecimal(36, 33), '0X_13')
+        self.assertEqual(self.converter.fromDecimal(36, 34), '0Y_12')
+        self.assertEqual(self.converter.fromDecimal(36, 35), '0Z_11')
+        print('\n', "test_decimal_36 passed")
+
+    def test_decimal_123(self):
+        self.assertEqual(self.converter.fromDecimal(123, 2), '02_1111011')
+        self.assertEqual(self.converter.fromDecimal(123, 8), '08_173')
+        self.assertEqual(self.converter.fromDecimal(123, 16), '0G_7B')
+        self.assertEqual(self.converter.fromDecimal(123, 26), '0Q_4J')
+        self.assertEqual(self.converter.fromDecimal(123, 33), '0X_3O')
+        self.assertEqual(self.converter.fromDecimal(123, 35), '0Z_3I')
+        print('\n', "test_decimal_123 passed")
+
+
+
 # BaseXNumber Testing code
 import unittest
 
@@ -412,11 +378,40 @@ class TestBaseXNumber(unittest.TestCase):
         self.assertFalse(num == 20)
         self.assertTrue(num != 20)
 
+
+# Main Q1
 if __name__ == '__main__':
+
+    converter = BaseConverter()
+
+    numbers = [36, 123, 456]
+
+    bases = [2, 8, 16]
+
+    for number in numbers:
+        print(f"\nTesting number {number}:")
+
+        currNumber = converter.represent(str(number), 10)
+
+        # Convert the number to each base
+        for base in bases:
+            print(f"\nConverting {currNumber} to base {base}:")
+            numInBase = converter.convert(currNumber, base)
+            print(f"Number {currNumber} in base {base} is {numInBase}")
+            currNumber = numInBase
+
+        print(f"\nConverting {currNumber} back to base 10:")
+        numInDecimal = converter.toDecimal(currNumber)
+        print(f"Number {currNumber} is {numInDecimal} in decimal")
+
+        # Check if the number is the same after converting back to base 10
+        if numInDecimal == number:
+            print(f"Success: {numInDecimal} == {numInDecimal}")
+        else:
+            print(f"Failure: {numInDecimal} != {numInDecimal}")
+
+
+    # Run the unit tests
     unittest.main()
-
-
-
-
 
 
