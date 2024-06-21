3ca6ee1daf1144d282aa77d21bc1bbd4fb727456
Nathan Devlin <ndevlin@alumni.upenn.edu>
2024-06-21 08:46:18 +0000

    Partially decrypting text

--- Rotator2_V3_Jun21.py
+++ Rotator2_V3_Jun21.py
@@ -17,18 +17,20 @@ class TwoLayerCaesarCipher:
         self.currentPosLayer2 = self.initialPosLayer2
     
     def encryptCharacter(self, char):
+        if char == "\n":
+            return "\n"
         ordChar = ord(char)
         ecryptedChar = ordChar + self.currentPosLayer1
-        if ecryptedChar > 0x7E:
+        while ecryptedChar > 0x7E:
             ecryptedChar = ecryptedChar - 0x5F
         ecryptedChar = ecryptedChar + self.currentPosLayer2
-        if ecryptedChar > 0x7E:
+        while ecryptedChar > 0x7E:
             ecryptedChar = ecryptedChar - 0x5F
         self.currentPosLayer1 += 1
-        if self.currentPosLayer1 > 0x7E:
+        while self.currentPosLayer1 > 0x7E:
             self.currentPosLayer1 = 0x20
             self.currentPosLayer2 += 1
-            if self.currentPosLayer2 > 0x7E:
+            while self.currentPosLayer2 > 0x7E:
                 self.currentPosLayer2 = 0x20
         newChar = chr(ecryptedChar)
         return newChar
@@ -40,20 +42,26 @@ class TwoLayerCaesarCipher:
         return result
 
     def decryptCharacter(self, char):
+        #print("charIn: ", char)
+        if char == "\n":
+            return "\n"
+        #if char == "u":
+            #print("We gotta u")
         self.currentPosLayer1 -= 1
         ordChar = ord(char)
         decryptedChar = ordChar - self.currentPosLayer2
-        if decryptedChar < 0x20:
+        while decryptedChar < 0x20:
             decryptedChar = decryptedChar + 0x5F
         decryptedChar = decryptedChar - self.currentPosLayer1
-        if decryptedChar < 0x20:
+        while decryptedChar < 0x20:
             decryptedChar = decryptedChar + 0x5F
-        if self.currentPosLayer1 < 0x20:
+        while self.currentPosLayer1 < 0x20:
             self.currentPosLayer1 = 0x7E
             self.currentPosLayer2 -= 1
-        if self.currentPosLayer2 < 0x20:
+        while self.currentPosLayer2 < 0x20:
             self.currentPosLayer2 = 0x7E
         newChar = chr(decryptedChar)
+        #print("charOut: ", newChar)
         return newChar
     
     def decrypt(self, text):
@@ -76,4 +84,31 @@ print(f"Encrypted: {encrypted}")
 decrypted = cypher.decrypt(encrypted)
 print(f"Decrypted: {decrypted}")
 
-print("Done")
+textFromFile = ''
+filePath = 'E2Rotor.txt'
+with open(filePath, 'r') as file:
+    textFromFile = file.read()
+
+print(textFromFile)
+
+print("\n")
+
+print("Brute Force Decryption: ")
+
+listOfDecryptedTexts = []
+
+for i in range(0x20, 0x7F):
+    for j in range(0x20, 0x7F):
+        cypher = TwoLayerCaesarCipher(j, i)
+        decrypted = cypher.decrypt(textFromFile)
+
+        numSpaces = 0
+        for char in decrypted:
+            if char == " ":
+                numSpaces += 1
+        if numSpaces > 94:
+            listOfDecryptedTexts.append(decrypted)
+            print(decrypted)
+            print(f"Initial Pos Layer 1: {j}", f"Initial Pos Layer 2: {i}")
+
+
